4422e8ffdf74364d171b78c6ac7fe504d153ce87
mhnpd <mhnpd@outlook.com>
2021-03-08 11:03:53 +0545

    added the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Robot Market</h1>
+      <h1>Mobile Market</h1>
       {/*Add your code here*/}
     </div>
   );
